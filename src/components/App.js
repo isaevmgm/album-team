25811aecddae2269c6aaed519521355585a7da84
baimag <baimag02@yandex.ru>
2021-02-13 15:17:36 +0300

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import {useDispatch} from "react-redux";
 import {useEffect} from "react";
 import {loadAlbums} from "../redux/actions";
+import Album from "./Album";
 
 function App() {
   const dispatch = useDispatch();
@@ -11,7 +12,7 @@ function App() {
 
   return (
     <div>
-
+      <Album/>
     </div>
   );
 }
